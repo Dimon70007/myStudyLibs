173346191c79130c6fd0390a6faab866e9741505
Отважный <dimon.fly.mail@mail.ru>
2016-09-15 17:47:37 +0300

    myHashSet has been refactored

--- src/mycollections/ArrayBasedMap.java
+++ src/mycollections/ArrayBasedMap.java
@@ -8,7 +8,7 @@ public class ArrayBasedMap<K, V> implements Map<K, V> {
     private double loadFactor;
     private final static double DEFAULT_LOADFACTOR=0.75;
     private int capacity;
-    private final static int DEFAULT_CAPACITY=Integer.MAX_VALUE/10000000;
+    private final static int DEFAULT_CAPACITY=Integer.MAX_VALUE/100000000;
     private final static int MULTIPLIER =10;
     private int threshold;
     private ArrayList<List<Pair>> values;
